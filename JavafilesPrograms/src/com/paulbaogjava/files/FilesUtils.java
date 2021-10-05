a9999094b38f338723106e553122ed7fdf338436
Bandipo Taiye <bandipotaiye@gmail.com>
2021-10-05 21:59:18 +0100

    Files method implemented

--- JavafilesPrograms/src/com/paulbaogjava/files/FilesUtils.java
+++ JavafilesPrograms/src/com/paulbaogjava/files/FilesUtils.java
@@ -1,8 +1,15 @@
 package com.paulbaogjava.files;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.logging.Logger;
 
 
@@ -31,6 +38,27 @@ public class FilesUtils {
 
 
     }
+    public static  void createAFile (final Path path, final FileAttribute<Set<PosixFilePermission>> fileAttribute){
+
+        if(Files.exists(path)){
+            logger.info("File already exist");
+
+        }else {
+            try {
+                Files.createFile(path, fileAttribute);
+                logger.info("file created");
+            } catch (IOException e) {
+                logger.warning(e.getMessage());
+            }
+        }
+
+
+
+
+
+
+    }
+
 
     public static long  getFileSize(final Path path){
         long  returnType = 0;
@@ -40,5 +68,84 @@ public class FilesUtils {
             e.printStackTrace();
         }
         return returnType;
+    }
+
+    public static void deleteAFile(final Path path){
+        boolean isFileDeleted = false;
+        try {
+             isFileDeleted = Files.deleteIfExists(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        logger.info( isFileDeleted ? " File is deleted": " file is not deleted");
+
+    }
+
+    public static void moveFile(final Path sourcePath, final Path destinationPath){
+
+        if(Files.exists(sourcePath) &&  !Files.exists(destinationPath)){
+
+            try {
+                Files.move(sourcePath, destinationPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        logger.info("File is moved from "
+                + sourcePath.toString()+" to "
+                + destinationPath.toString());
+    }
+
+    public static void readFileUsingReadAllLine(final Path path){
+
+        List<String> content = null;
+
+       if(Files.exists(path)){
+           try {
+              content = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+           } catch (IOException e) {
+               logger.warning(e.getMessage());
+           }
+       }
+
+        if (content != null) {
+            content.forEach(System.out::println);
+        }
+
+    }
+
+    public static Optional< List<String>> readFileUsingReadAllLines(final Path path){
+
+        List<String> content = null;
+
+        if(Files.exists(path)){
+            try {
+                content = Files.readAllLines(path);
+
+            } catch (IOException e) {
+                logger.warning(e.getMessage());
+            }
+        }
+
+
+
+        return Optional.ofNullable(content);
+    }
+
+    public static void writeToFile(List<String> contentToWrite, Path pathToWrite){
+
+        if(contentToWrite != null){
+            try {
+                Files.write(pathToWrite, contentToWrite, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+            } catch (IOException e) {
+                logger.warning(e.getMessage());
+            }
+        }
+
+        logger.info("File successfully written");
     }
 }
--- JavafilesPrograms/src/com/paulbaogjava/files/JavaFileApp.java
+++ JavafilesPrograms/src/com/paulbaogjava/files/JavaFileApp.java
@@ -1,14 +1,14 @@
 package com.paulbaogjava.files;
 
-import java.io.IOException;
-import java.nio.file.Files;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFileAttributes;
+
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -38,6 +38,41 @@ public class JavaFileApp {
 
         LOGGER.info("file : " + fileSize );
         LOGGER.info("file 1: "+fileSize1);
+
+        //Deleting a file
+        Path path3 = Paths.get(FileConstants.FILE_PATH+"bugs3.txt");
+        FilesUtils.createAFile(path3, fileAttribute);
+
+//        FilesUtils.deleteAFile(path3);
+
+        //Moving a file: the source file is deleted once it has been moved to the destination
+
+        Path path4 = Paths.get(FileConstants.FILE_PATH+"bugs4.txt");
+
+        FilesUtils.moveFile(path2, path4 );
+
+
+        //Reading a file
+
+        System.out.println();
+        FilesUtils.readFileUsingReadAllLine(path4);
+
+
+        //Writing to a file
+
+        Path path5 = Paths.get(FileConstants.FILE_PATH+"bugs5.txt");
+
+        List<String> contents = FilesUtils.readFileUsingReadAllLines(path4)
+                .orElseThrow(
+                        () -> new NullPointerException(" No file to read")
+                );
+
+        FilesUtils.writeToFile(contents, path5);
+
+        System.out.println();
+        FilesUtils.readFileUsingReadAllLine(path5);
+
+
 
 
 
